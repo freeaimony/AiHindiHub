363205d4140f9705b7f0cb8290565c31d21cf332
Amit Kumar <115913686+freeaimony@users.noreply.github.com>
2026-07-09 04:13:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-function showMessage() {
-  alert("Welcome to AIHindiHub 🚀");
+function toggleDarkMode(){
+document.body.classList.toggle("dark");
 }
 
-window.onload = function () {
-  console.log("AIHindiHub Loaded Successfully");
-};
+function searchSite(){
+let q=document.getElementById("searchInput").value.toLowerCase();
+
+if(q.includes("chatgpt")){
+alert("ChatGPT article coming soon!");
+}else if(q.includes("gemini")){
+alert("Google Gemini article coming soon!");
+}else{
+alert("More articles will be added soon.");
+}
+}
